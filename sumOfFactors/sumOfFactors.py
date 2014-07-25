43e4d3a3e04d675c5a6081597e9d58b6e98ed623
Dominik Szerszen <dominik.sze@gmail.com>
2014-07-25 15:40:56 +0200

    Refactoring

--- sumOfFactors/sumOfFactors.py
+++ sumOfFactors/sumOfFactors.py
@@ -1,34 +1,46 @@
 import profile
 
-def find1(num):
+def find1(n):
+	""" 
+	Returns sum of proper divisors of n (nbers less than n which divide evenly into n).
+	Running time: O(n)
+	"""
 	sum = 1
-	for i in range(2, num):
-		if num%i==0:
+	for i in range(2, n):
+		if n%i==0:
 			sum+=i
-	print(sum)
+	return sum
 
-def find2(num):
+def find2(n):
+	""" 
+	Returns sum of proper divisors of n (nbers less than n which divide evenly into n).
+	Running time: O(n) (better constant than find1(n))
+	"""
 	sum = 1
-	for i in range(2, int(num/2.)+1):
-		if num%i==0:
+	for i in range(2, int(n/2.)+1):
+		if n%i==0:
 			sum+=i
-	print(sum)
+	return sum
 
-def find3(num):
+def find3(n):
+	""" 
+	Returns sum of proper divisors of n (nbers less than n which divide evenly into n).
+	Running time: O(sqrt(n))
+	"""
 	import math
 	sum = 1
 	limit = 1
-	sqrt = int(math.sqrt(num))
-	if num == sqrt*sqrt:
+	sqrt = int(math.sqrt(n))
+	if n == sqrt*sqrt:
 		sum+=sqrt
 		limit-=1
-	for i in range(2, int(math.sqrt(num))+limit):
-		if num%i==0:
-			sum+=(i+num/i)
-	print(int(sum))
+	for i in range(2, int(math.sqrt(n))+limit):
+		if n%i==0:
+			sum+=(i+n/i)
+	return sum
 
 if __name__=='__main__':
-	num = 29629042
-	profile.run("find1(num)")
-	profile.run("find2(num)")
-	profile.run("find3(num)")
+	n = 29629042
+	profile.run("find1(n)")
+	profile.run("find2(n)")
+	profile.run("find3(n)")
