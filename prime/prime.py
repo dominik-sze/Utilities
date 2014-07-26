6e7b4d9bde5ace78f653f14b1b8be2bb8c243645
Dominik Szerszeń <dominik.sze@gmail.com>
2014-07-27 00:12:27 +0200

    Refactoring

--- prime/prime.py
+++ prime/prime.py
@@ -1,4 +1,5 @@
 import profile
+import unittest
 
 def eratosthenes_sieve1(upper_limit):
 	"""
@@ -23,28 +24,34 @@ def eratosthenes_sieve2(upper_limit):
 	"""
 	Returns a list of primes < n
 	"""
-
-	sieve = [True]*upper_limit
-	sieve[0] = False
-	sieve[1] = False
-	for i in range(3, int(upper_limit**.5)+1, 2):
-		if sieve[i]:
-			for j in range(i*i, upper_limit, 2*i):
-				sieve[j] = False
-	return [2] + [p for p in range(3, n, 2) if sieve[p]]
-
-def eratosthenes_sieve3(upper_limit):
-	"""
-	Returns a list of primes < n
-	"""
 	sieve = [True]*upper_limit
 	for i in range(3, int(upper_limit**0.5)+1, 2):
 		if sieve[i]:
-			sieve[i*i::2*i] = [False]*int(((n-i*i-1)/(2*i)+1))
-	return [2] + [i for i in range(3, n, 2) if sieve[i]]
+			sieve[i*i::2*i] = [False]*int(((upper_limit-i*i-1)/(2*i)+1))
+	return [2] + [i for i in range(3, upper_limit, 2) if sieve[i]]
+
+class PrimesTest(unittest.TestCase):
+	def setUp(self):
+		self.__primes = []
+		try:
+			with open('primes.txt') as fp:
+				for line in fp:
+					line = line.rstrip()
+					self.__primes.extend([int(x) for x in line.split(' ')])
+		except IOError as e:
+			print('I/O error ({}): {}'.format(e.errno, e.strerror))
+
+	def test_sieve(self):
+		last = self.__primes[-1]
+		result = eratosthenes_sieve2(last+1)
+		
+		for i,v in enumerate(result):
+			self.assertEqual(self.__primes[i], v)
+
 
 if __name__=='__main__':
 	n = 2000000
 	profile.run("eratosthenes_sieve1(n)")
 	profile.run("eratosthenes_sieve2(n)")
-	profile.run("eratosthenes_sieve3(n)")
+
+	unittest.main()
